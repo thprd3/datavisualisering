91f99f8bc98bc3db46b87798dab1aee2c881e6ed
thprd3 <84687139+thprd3@users.noreply.github.com>
2025-02-13 13:29:16 +0100

    basic setup

--- README.md
+++ README.md
@@ -1,45 +1,6 @@
+Her skal jeg lage datavisualiseringer ved hjelp av Chart.js og Matplotlib. Grafene skal vises på web via Flask, og baseres på data i en SQL-database.
+
 to-do:
-* jinja templating basic [x]
-* routing [x]
-* sign up + login [x]
-* sqlite [x]
-* GET + POST på samme route [x]
-* hash pw [x]
-* custom CLI commands [x]
-* static files (eg CSS + JS) [x]
-* avansert db connection (while-kontekst) [x]
-* dynamiske ruter - jinja-variabler [x]
-* session [x]
-* tilgangsstyrt admin page [x]
-* .env-fil for db connection [x]
-* flash for midlertidige tilbakemeldinger via session [x]
-* manuelt returnere statuskoder [x]
-* global, custom feilhåndtering med @app.erorrhandler() [x]
-* pymysql vs mysql.connector [x] 
-* soft delete [x]
-* lokal mariadb-versjon [ ] 
-* mysql workbench [ ]
-* Waitress for webserver [ ]
-* Nginx for reverse proxy [ ]
-
-maybe:
-* sqlalchemy [~]
-* phpmyadmin [~]
-* requirements.txt - inkl automatisk generering?
-* validate sign up w regex [ ]
-* js frontend...? hva mener jeg her - kombinere Flask med et JS-rammeverk?
-* fil upload
-* API. Flask-RESTful?
-* Flask-SocketIO for real time?
-* venv...
-* e-post med Flask-Mail
-* multiple Flask-server til en Nginx??
-* docker image :) og teste på flere maskiner
-* logging av feil til fil
-* e-post ved 500-feil
-* gunicorn for hosting fra linux
-* flask_limiter for rate limiting
-* automatisk backups av db
-
-setup:
-* .env med SECRET_KEY og DATABASE_INFO
+* datagrunnlag i SQL
+* datavis med chart.js
+* datavis med matplotlib
--- app.py
+++ app.py
@@ -1,13 +1,9 @@
-from flask import Flask, render_template, request, session, flash, redirect
-from dotenv import load_dotenv
+from flask import Flask, render_template
 import os
 import sqlite3
-import bcrypt
 
 app = Flask(__name__)
-load_dotenv()
-app.secret_key = os.getenv("SECRET_KEY", "default_key") # parameter 1 = nøkkel i .env, parameter 2 = fallback-verdi. kalles ODP :)
-db_info = os.getenv("DATABASE_INFO", "foo.db")
+db_info = "app.db"
 
 def init_db():
     with sqlite3.connect(db_info) as conn:
@@ -24,150 +20,19 @@ def init_db():
 
 init_db()
 
-@app.errorhandler(404)
-def not_found():
-    return render_template('404.html'), 404
-
-@app.errorhandler(500)
-def RIP_server():
-    return "diesofcringe", 500
-
-@app.context_processor # dette kjører før template renderes, er tilgjengelig på alle ruter
-def current_user():
-    return {
-        "current_user": session.get("user"),
-        "current_role": session.get("role")
-    }
-
 @app.cli.command("reset") # kjører med flask reset
 def reset():
     if os.path.exists(db_info):
         os.remove(db_info)
 
-def hash_password(password):
-    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()) # bcrypt trenger passord som bytes, ikke tekst
-
-def register_user(username, password, role):
-    with sqlite3.connect(db_info) as conn:
-        cursor = conn.cursor()
-        sql = 'SELECT * FROM users WHERE username = ?'
-        cursor.execute(sql, (username,)) # må sende tuple selv om bare 1 verdi
-        if cursor.fetchone():
-            flash(f"Username '{username}' already in use!")
-            return redirect('/register'), 409 # 409 = conflict
-        else:
-            hashed_password = hash_password(password)
-            if role == "admin":
-                sql = "INSERT INTO users (username, password, role) VALUES (?, ?, ?)"
-                cursor.execute(sql, (username, hashed_password, role))
-            else:
-                sql = "INSERT INTO users (username, password) VALUES (?, ?)"
-                cursor.execute(sql, (username, hashed_password))
-            conn.commit()
-            flash(f"User '{username}' of role '{role}' registered.")
-            return redirect ('/login')
-
-def login_user(username, password):
-    with sqlite3.connect(db_info) as conn:
-        cursor = conn.cursor()
-        sql = "SELECT * FROM users WHERE username = ?"
-        cursor.execute(sql, (username,))
-        result = cursor.fetchone()
-        
-        if result:
-            hash = result[2] # id, username, password
-            if bcrypt.checkpw(password.encode('utf-8'), hash):
-                session["user"] = username
-                session["role"] = result[3]
-                return redirect('/login')
-            else:
-                return "Invalid password", 401
-        else: 
-            flash(f"No user found with username '{username}' and the provided password.")
-            return redirect('/login'), 401
-
-def soft_delete_user(username):
-    conn = sqlite3.connect("app.db")
-    cursor = conn.cursor()
-    sql = "SELECT * FROM users WHERE username = ?"
-    cursor.execute(sql, (username,))
-    if cursor.fetchone():
-        sql = "UPDATE users SET is_active = 'no' WHERE username = ?"
-        cursor.execute(sql, (username))
-        conn.commit()
-        conn.close()
-        return render_template("home.html", message=f"User '{username}' has been soft deleted.")
-    else:
-        conn.close()
-        return render_template("delete.html", message=f"No user found with username '{username}'.")
-
 @app.route('/')
 def index_page():
-    return redirect('/login')
-
-@app.route('/register', methods=["GET", "POST"])
-def register_page():
-    print(f"{request.method} received at {request.path}")
-    if request.method == 'GET':
-        return render_template("signup.html")
-    else:
-        username = request.form.get("username")
-        password = request.form.get("password")
-        role = request.form.get("role")
-        return register_user(username, password, role)
-
-@app.route('/login', methods=["GET", "POST"])
-def login_page():
-    print(f"{request.method} received at {request.path}")
-    if request.method == 'GET':
-        return render_template("login.html")
-    else:
-        username = request.form.get("username")
-        password = request.form.get("password")
-        return login_user(username, password)
-
-@app.route('/logout')
-def logout():
-    session.pop("user", None) #fjerner nøkkelen hvis den finnes, men gjør ingenting hvis den ikke eksisterer. "Optional Default Parameter", veldig Python-idiomatisk
-    session.pop("role", None)
-    return redirect('/login')
-
-@app.route("/admin")
-def admin_page():
-    if session.get("role") == "admin":   
-        with sqlite3.connect(db_info) as conn:
-            cursor = conn.cursor()
-            sql = "SELECT * FROM users"
-            cursor.execute(sql)
-            users = cursor.fetchall()
-            return render_template("admin.html", users = users)
-    else:
-        flash("Only admins can access /admin")
-        return redirect('/login'), 403 # 403 = forbidden
-
-@app.route("/profile")
-def user_page():
-    if "user" in session:
-        with sqlite3.connect(db_info) as conn:
-            cursor = conn.cursor()
-            sql = "SELECT * FROM users WHERE username = ?"
-            cursor.execute(sql, (session.get("user"),))
-            user_info = cursor.fetchone()
-            return render_template("profile.html", user_info = user_info)
-    else: 
-        flash("Must be logged in to access /profile")
-        return redirect("/login"), 403
-
-@app.route("/delete", methods=["GET", "POST"])
-def delete_page():
-    if request.method=="GET": 
-        if session.get("role") == "admin":
-            return render_template("delete.html")
-        else:
-            return render_template("home.html", message="Only admins can access /delete.")
-    elif request.method=="POST":
-        username = request.form.get("username")
-        return soft_delete_user(username)
+    with sqlite3.connect(db_info) as conn:
+        cursor = conn.cursor()
+        sql = "SELECT * FROM users"
+        cursor.execute(sql)
+        users = cursor.fetchall()
+        return render_template("index.html")
 
 if __name__ == '__main__':
     app.run()
